c03a1829a9f7f53dc4e83656c719838a692e02ce
raynecloudy <contact@raynec.dev>
2025-12-26 23:51:22 -0800

    account creation endpoint

---
+++ src/routes/create/+server.ts
@@ -0,0 +1,26 @@
+import cryptoRandomString from "crypto-random-string";
+import { db } from "../hooks.server";
+import { error } from "@sveltejs/kit";
+import { hash } from "bcrypt";
+import type { RequestHandler } from "./$types";
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+  const { password, username } = await request.json();
+
+  if (typeof password !== "string") throw error(400, "Password is not a string");
+  if (typeof username !== "string") throw error(400, "Username is not a string");
+
+  if (username !== username.replace(/\s/gmi, "")) throw error(400, "Username contains whitespace characters");
+
+  if (username.length < 1) throw error(400, "Username too short (minimum 1 character)");
+  if (username.length > 30) throw error(400, "Username too long (maximum 30 characters)");
+  if (password.length < 8) throw error(400, "Password too short (minimum 8 characters)");
+  if (password.length > 60) throw error(400, "Password too long (maximum 60 characters)");
+
+  if (await db.run("SELECT * FROM accounts WHERE username = ?", [username])) throw error(400, "Username is taken");
+  else {
+    const authCode = cryptoRandomString({ length: 100 });
+    await db.run("INSERT INTO accounts (authCode, joined, password, username) VALUES (?, ?, ?, ?)", [authCode, Date.now(), await hash(password, 10), username]);
+    return new Response(authCode);
+  }
+};
--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -1,9 +1,14 @@
-import { Database } from "sqlite3";
+import sqlite3 from "sqlite3";
+const { Database } = sqlite3;
+import { AsyncDatabase } from "promised-sqlite3";
 
-export const db = new Database("./database/database.sqlite3");
+const _db = new Database("./database/database.sqlite3");
+export const db = new AsyncDatabase(_db);
 
 db.run(`CREATE TABLE IF NOT EXISTS accounts (
+  authCode TEXT,
   banReason TEXT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   joined INTEGER NOT NULL,
   name TEXT,
   password TEXT NOT NULL,
