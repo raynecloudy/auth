a91c6292ba19b5d739af33ec3089eb74bcd95c27
raynecloudy <contact@raynec.dev>
2025-12-26 21:12:27 -0800

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>auth.rayne.page</h1>
+<h2>What is this?</h2>
+<p>This service handles and centralizes authentication across projects I own. It gives you a persistent identifier usable throughout my ecosystem.</p>
+<div class="action-row">
+  <a href="/create" class="button">Create account</a>
+  <a href="/auth" class="button">Log in</a>
+</div>
