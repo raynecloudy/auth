cddb0fed06fad537102211af860c24fa2b75ea7d
raynecloudy <contact@raynec.dev>
2025-12-26 20:53:23 -0800

    basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href="/style.css">
 </svelte:head>
 
-{@render children?.()}
+<main>
+	{@render children?.()}
+</main>
