afbad7369dd007e4a0f4729f1a876c6d6272ed1a
raynecloudy <contact@raynec.dev>
2025-12-26 21:43:52 -0800

    account creation page

---
+++ src/routes/create/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let email = $state("");
+  let password = $state("");
+  let read = $state(false);
+</script>
+
+<h1>Create account</h1>
+<form>
+  <div>
+    <label for="email">Email</label>
+    <input type="email" id="email" bind:value={email} oninput={() => {
+      email = email.replace(/\s/gmi, "");
+    }} required>
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input type="password" id="password" bind:value={password} required>
+  </div>
+  <div>
+    <input type="checkbox" id="read" bind:checked={read}><label for="read">I have read and agree to the <a href="/terms">Terms of Service</a> and the <a href="/privacy">Privacy Policy</a></label>
+  </div>
+  <input type="submit" value="Create account" class="button" disabled={!read}>
+</form>
